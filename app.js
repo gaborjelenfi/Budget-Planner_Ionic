7ecf7e55d5c846cf4167a87850f6036b5554d037
Jelenfi Gábor <dzsub4@gmail.com>
2021-08-09 21:17:34 +0200

    simple budget planner app

---
+++ app.js
@@ -0,0 +1,45 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('#alert');
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
+
+confirmBtn.addEventListener('click', () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (
+        enteredReason.trim().length <= 0 ||
+        enteredAmount <= 0 ||
+        enteredAmount.trim().length <= 0
+    ) {
+        const alert = document.createElement('ion-alert');
+        alert.header = 'Invalid inputs!';
+        alert.message = 'Please enter valid reason and amount!';
+        alert.buttons = ['Okay'];
+        alertCtrl.appendChild(alert);
+        alert.present();
+        
+        return;
+    }
+
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    expensesList.appendChild(newItem);
+
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses.toFixed(2);
+    clear();
+});
+
+cancelBtn.addEventListener('click', clear);
